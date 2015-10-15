a3f880100404fc9eba9efaea0f447daefa655060
wackyzacky42 <wacky_zacky@hotmail.com>
2015-10-15 09:16:44 -0700

    101515 - AM changes
    - Tinkered with Selenium navigation, mostly failed. Can get to Player
    Search but cannot get email field to populate.  :/

--- TkinterTest.py
+++ TkinterTest.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 # from Tkinter import *
 
 
@@ -24,16 +27,34 @@ from selenium.webdriver.common.keys import Keys
 # root.mainloop()
 
 driver = webdriver.Firefox()
+# driver = webdriver.Chrome()
 actions =  ActionChains(driver)
-driver.get("http://auth-main.motiga.net:83/0.0/login")
-# assert "Log In" in driver.title
+driver.implicitly_wait(10)
+# driver.get("http://auth-main.motiga.net:83/0.0/login")
+# elem = driver.find_element_by_name("login")
+# elem.send_keys("zackn@motiga.com")
+# elem = driver.find_element_by_name("password")
+# elem.send_keys("password")
+# elem.send_keys(Keys.RETURN)
+# driver.get("http://auth-main.motiga.net:83/0.0/player_search")
+# driver.navigate().to("http://www.google.com")
+# elem = driver.find_element_by_xpath("/0.0/player_search")
+#elem = driver.find_element_by_id("environment")
+#print(elem)
+driver.get("http://auth-main.motiga.net:83/0.0/player_search")
 elem = driver.find_element_by_name("login")
 elem.send_keys("zackn@motiga.com")
 elem = driver.find_element_by_name("password")
 elem.send_keys("password")
 elem.send_keys(Keys.RETURN)
-driver.find_element_by_xpath("//a[contains(@href,'/0.0/player_search')]")
-actions.click()
+elem = driver.find_element_by_id("email-search")
+print(elem)
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "email-search")))
+elem.send_keys("zackn@motiga.com")
+elem.send_keys(Keys.RETURN)
+
+
+# actions.click()
 # assert "Nominations" in driver.title
 # driver.get("http://auth-main.motiga.net:83/0.0/player_search")
 
